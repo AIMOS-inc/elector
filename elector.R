6d1cf4920415b7133576a5b0e222f7bad847cf20
Alex O. Holcombe <aoholcombe@gmail.com>
2021-11-20 06:04:26 +1100

    small comments etc., about to change to just one election rather than separate elections for president, treasurer, etc.

--- elector.R
+++ elector.R
@@ -1,79 +1,78 @@
-library(STV)
+library(STV) #Single Transferable Vote Counting.
 library(tidyverse)
-library(misinformation) #remotes::install_github("Lingtax/misinformation)
+library(misinformation) #devtools::install_github("lingtax/misinformation")
+#remotes::install_github("Lingtax/misinformation")
 
+# Election rules are in the by-laws at the website aimos.community
+
+#2021 is the first year where members are elected to the Board rather than to specific offices
+#all 9 spots are up for grabs.
 
 # Election function (set defaults) ----------------------------------------
-
 aimos_stv <-  function(ballots, seats) {
-  
-  stv(ballots,
-      seats = seats, 
-      surplusMethod = "Fractional", 
+
+  stv( #Analyze a data frame of STV election ballot rankings and return the elected voters and details of the steps in the election counting.
+      ballots,
+      seats = seats,  #How many positions are there to fill with this vote
+      surplusMethod = "Fractional",
       quotaMethod = "Droop")
-  
 }
 
 
 # Read ballots ------------------------------------------------------------
-
 meta <-  read_csv("meta_election.csv")
-df <-  read_qualtrics("AIMOS_2020_election_Test.csv")
-df <- df %>% meta_rename(meta, old =  name_raw, new = name_clean)
+df <-  misinformation::read_qualtrics("AIMOS_2020_election_Test.csv")
+dg <- df %>% meta_rename(meta, old =  name_raw, new = name_clean)
 
 # President ---------------------------------------------------------------
-
-df %>% 
-  select(starts_with("pres")) %>% 
-  as.data.frame() %>% 
+dg %>%
+  select(starts_with("pres")) %>%
+  as.data.frame() %>%
   aimos_stv(seats = 1)
 
 
 # Vice-president ----------------------------------------------------------
-
-df %>% 
-  select(starts_with("vicepres")) %>% 
-  as.data.frame() %>% 
+dg %>%
+  select(starts_with("vicepres")) %>%
+  as.data.frame() %>%
   stv(seats = 1)
 
 # Treasurer  ----------------------------------------------------------
-
-df %>% 
-  select(starts_with("treas")) %>% 
-  as.data.frame() %>% 
+dg %>%
+  select(starts_with("treas")) %>%
+  as.data.frame() %>%
   stv(seats = 1)
 
 # Secretary ----------------------------------------------------------
 
-df %>% 
-  select(starts_with("sec")) %>% 
-  as.data.frame() %>% 
+dg %>%
+  select(starts_with("sec")) %>%
+  as.data.frame() %>%
   stv(seats = 1)
 
 # Student representative ----------------------------------------------------------
 
-df %>% 
-  select(starts_with("student")) %>% 
-  as.data.frame() %>% 
+dg %>%
+  select(starts_with("student")) %>%
+  as.data.frame() %>%
   stv(seats = 1)
 
 # D & I ----------------------------------------------------------
 
-df %>% 
-  select(starts_with("div")) %>% 
-  as.data.frame() %>% 
+dg %>%
+  select(starts_with("div")) %>%
+  as.data.frame() %>%
   stv(seats = 1)
 
 # General ----------------------------------------------------------
 
 # provide names of elected candidates after previous rounds
 # elected <- c("", "")
-
-df %>% 
-  select(starts_with("gen")) %>% 
+dg %>%
+  select(starts_with("gen")) %>%
   select(-contains(elected)) %>%
-  as.data.frame() %>%  
-  cleanBallots() %>% 
+  as.data.frame() %>%
+  cleanBallots() %>%
   stv(seats = 6)
 
 
