e0b40670ce48284ee1744c17af187bef90fe1daa
Alex O. Holcombe <aoholcombe@gmail.com>
2021-12-02 06:18:42 +1100

    done 2021

--- elector.R
+++ elector.R
@@ -16,29 +16,36 @@ aimos_stv <-  function(ballots, seats) {
       quotaMethod = "Droop")
 }
 
-#NOT SURE CHOICE RANDOMSIATION WILL WORK
+#Confirmed that turning on choice randomisation in Qualtrics doesn't screw it up
 
 # Read ballot columnn names file, used only for renaming qualtrics file column names
 meta <-  read_csv("meta_election.csv")
 
 data_path <- here("data")
 #"AIMOS_2020_election_Test.csv"
-votes_file <- file.path(data_path, "AIMOS2021electionRandomiseResponses_test.csv")
+votes_file <- file.path(data_path, "AIMOS2021electionRandomiseResponses_December 1, 2021_03.29.csv")
 df <-  misinformation::read_qualtrics(votes_file)
 
 # Use Ling's function to rename the columns from the qualtrics file
 dg <- df %>% misinformation::meta_rename(meta, old =  name_raw, new = name_clean)
 
 # General election------
-
 ballots<- dg %>%
   select(starts_with("g_")) %>% #general seats, which is the only kind there is for AIMOS from 2021
   as.data.frame()
 
 #Validate ballots. Must be dataframe, not tibble
-validateBallots(ballots)
-#cballots <- cleanBallots(ballots)
+#validateBallots(ballots)  #Didn't work without cleaning first with real data because said
+## [1] "Row(s): 6 do not contain any ranks."
+## Error in validateBallots(ballots) :
+## Please remove blank row(s), or use cleanBallots()
+# I think that happened because when I first started the survey I didn't require responses
 
-results <- ballots %>%  stv(seats = 3)
+cballots <- cleanBallots(ballots)
+validateBallots(cballots)
+
+results <- cballots %>%  stv(seats = 9)
 results$elected
 
+#Sanity check by assessing mean rank to each candidate
+colMeans(cballots)
